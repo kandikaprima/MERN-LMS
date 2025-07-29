b1ae6f603f794f0a894b04f1626bcdd27cda66bc
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-07-30 01:47:18 +0800

    feat: add manager or student logic

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,7 +3,9 @@ import Sidebar from "./sidebar";
 import Header from "./header";
 import { Outlet, useMatch } from "react-router-dom";
 
-export default function LayoutDashboard() {
+export default function LayoutDashboard({
+  isAdmin = true
+}) {
   const isPreviewPage = useMatch('/manager/courses/:id/preview');
 
   return (
@@ -13,7 +15,7 @@ export default function LayoutDashboard() {
         <Outlet />
       ) :(
         <div className="flex min-h-screen">
-        <Sidebar />
+        <Sidebar isAdmin={isAdmin}/>
         <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
           <Header />
           <Outlet />
