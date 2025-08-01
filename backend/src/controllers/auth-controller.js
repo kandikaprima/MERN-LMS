d620f22886969e94e6a77932c03c38ae3dd00f59
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-08-01 21:20:54 +0800

    feat(controller): add payment integration with Midtrans on user signup

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,7 +1,11 @@
 import bcrypt from "bcrypt";
 import userModel from "../models/user-model.js";
+import transactionModel from "../models/transaction-model.js";
 
 export const signUpAction = async (req, res) => {
+    const midtransUrl = process.env.MIDTRANS_URL
+    const midtransAuthString = process.env.MIDTRANS_AUTH_STRING
+
     try {
         const body = req.body //name, email, password
 
@@ -16,13 +20,43 @@ export const signUpAction = async (req, res) => {
         })
 
         // Action payment gateway midtrans
+        const transaction = new transactionModel({
+            user: user._id,
+            price: 280000
+        })
+
+        const midtrans = await fetch(midtransUrl, {
+            method: 'POST',
+            body: JSON.stringify({
+                transaction_details: {
+                    order_id: transaction._id.toString(),
+                    gross_amount: transaction.price
+                },
+                credit_card:{
+                    secure : true
+                },
+                customer_details: {
+                    email: user.email,
+                },
+                callbacks: {
+                    finish: 'http://localhost:5173/succes-checkout'
+                }
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Basic ${midtransAuthString}`
+            }
+        })
+
+        const resMidtrans = await midtrans.json()
 
         await user.save()
+        await transaction.save()
 
         return res.json({
             message: 'Sign Up Succes',
             data: {
-                midtrans_payment_url: 'https://google.com'
+                midtrans_payment_url: resMidtrans.redirect_url
             }
         })
     } catch (error) {
