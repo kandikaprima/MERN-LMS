efd5b0d0aed31bd16160a1e35f6cbcc62f49ddc1
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-08-03 01:47:59 +0800

    feat(schema): add zod validation schema for sign up form

--- frontend/src/utils/zod-schema.js
+++ frontend/src/utils/zod-schema.js
@@ -1,9 +1,5 @@
 import z from 'zod'
 
-export const exampleSchema = z.object({
-    name: z.string().min(3)
-})
-
 export const signUpSchema = z.object({
     name: z.string().min(5),
     email: z.string().email(),
