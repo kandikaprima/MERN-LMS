55b6a1d1f5dfd13790fc0b5a6a0349312df7b331
Kandika Prima Putra <84494833+kandikaprima@users.noreply.github.com>
2025-08-03 01:43:41 +0800

    feat(route): add POST /sign-in route with validation

--- backend/src/routes/auth-routes.js
+++ backend/src/routes/auth-routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { validateRequest } from "../middlewares/validate-request.js";
-import { signUpSchema } from "../utils/schema.js";
-import { signUpAction } from "../controllers/auth-controller.js";
+import { signInSchema, signUpSchema } from "../utils/schema.js";
+import { signInAction, signUpAction } from "../controllers/auth-controller.js";
 
 const authRoutes = express.Router()
 
 authRoutes.post('/sign-up', validateRequest(signUpSchema), signUpAction)
+authRoutes.post('/sign-in', validateRequest(signInSchema), signInAction)
 
 export default authRoutes
