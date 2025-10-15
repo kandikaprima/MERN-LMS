7788df3834a971d582bd8f5c8cfb2059be4d42b6
Kandika Prima Putra <kandikaprima@gmail.com>
2025-10-15 16:08:13 +0800

    style(sing-up): adjust error message spacing in SignUp form

--- frontend/src/pages/sign-up/index.jsx
+++ frontend/src/pages/sign-up/index.jsx
@@ -54,17 +54,17 @@ export default function SignUpPage() {
                             <img src="/assets/images/icons/user-octagon-white.svg" className="w-6 h-6 flex shrink-0" alt="icon" />
                             <input type="text" name="name" id="name" className="appearance-none outline-none !bg-transparent w-full font-semibold text-white placeholder:font-normal placeholder:text-[#6B6C7F]" placeholder="Write your complete name" {...register('name')}/>
                         </div>
-                        {errors.name?.message && <p className="text-red-500 text-xs -m-5">{errors.name?.message}</p>}
+                        {errors.name?.message && <p className="text-red-500 text-xs -mt-5">{errors.name?.message}</p>}
                         <div className="flex items-center gap-3 w-full rounded-full border p-[14px_20px] transition-all duration-300 focus-within:border-[#8661EE] focus-within:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
                             <img src="/assets/images/icons/sms-white.svg" className="w-6 h-6 flex shrink-0" alt="icon" />
                             <input type="email" name="email" id="email" className="appearance-none outline-none !bg-transparent w-full font-semibold text-white placeholder:font-normal placeholder:text-[#6B6C7F]" placeholder="Write your email address" {...register('email')}/>
                         </div>
-                        {errors.email?.message && <p className="text-red-500 text-xs -m-5">{errors.email?.message}</p>}
+                        {errors.email?.message && <p className="text-red-500 text-xs -mt-5">{errors.email?.message}</p>}
                         <div className="flex items-center gap-3 w-full rounded-full border p-[14px_20px] transition-all duration-300 focus-within:border-[#8661EE] focus-within:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
                             <img src="/assets/images/icons/key-white.svg" className="w-6 h-6 flex shrink-0" alt="icon" />
                             <input type="password" name="password" id="password" className="appearance-none outline-none !bg-transparent w-full font-semibold text-white placeholder:font-normal placeholder:text-[#6B6C7F]" placeholder="Type your secure password" {...register('password')}/>
                         </div>
-                        {errors.password?.message && <p className="text-red-500 text-xs -m-5">{errors.password?.message}</p>}
+                        {errors.password?.message && <p className="text-red-500 text-xs -mt-5">{errors.password?.message}</p>}
                         <hr className="border-[#262A56]" />
                         <button type="submit" className="w-full rounded-full border p-[14px_20px] text-center font-semibold text-white bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
                             Sign Up Now
