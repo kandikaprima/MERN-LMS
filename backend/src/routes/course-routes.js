2c9d491707d763284a053ffbc0783dc9a536ac60
Kandika Prima Putra <kandikaprima@gmail.com>
2025-10-28 20:05:24 +0800

    feat(course-routes): add post and put routes with multer for course management

--- backend/src/routes/course-routes.js
+++ backend/src/routes/course-routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getCourses } from "../controllers/course-controller.js";
+import { getCourses, postCourse, updateCourse } from "../controllers/course-controller.js";
 import { verifyToken } from "../middlewares/verify-token.js";
+import multer from "multer";
+import { fileFilter, fileStorageCourse } from "../utils/multer.js";
 
 const courseRoutes = express.Router()
 
+const upload = multer({
+    storage: fileStorageCourse,
+    fileFilter
+})
+
 courseRoutes.get('/courses', verifyToken, getCourses)
+courseRoutes.post('/courses', verifyToken, upload.single('thumbnail'), postCourse)
+courseRoutes.put('/courses', verifyToken, upload.single('thumbnail'), updateCourse)
 
 export default courseRoutes
