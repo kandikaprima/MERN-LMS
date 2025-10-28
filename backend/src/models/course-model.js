0ac62ca902309e4efa906290211a2d614ba28318
Kandika Prima Putra <kandikaprima@gmail.com>
2025-10-28 20:03:48 +0800

    fix(course-model): change students field to array of ObjectIds for multiple student references

--- backend/src/models/course-model.js
+++ backend/src/models/course-model.js
@@ -21,10 +21,12 @@ const courseModel = mongoose.Schema({
         type: String,
         required: true
     },
-    students: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
+    students: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
     manager: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
