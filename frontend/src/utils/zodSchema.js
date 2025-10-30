2148ee49a88bc7480a4a9d1489ce78f71e350749
Kandika Prima Putra <kandikaprima@gmail.com>
2025-10-30 22:34:47 +0800

    feat(validation): add zod schema for course creation and update

--- frontend/src/utils/zodSchema.js
+++ frontend/src/utils/zodSchema.js
@@ -7,3 +7,15 @@ export const signUpSchema = z.object({
 })
 
 export const signInSchema = signUpSchema.omit({name: true})
+
+export const createCourseSchema = z.object({
+    name: z.string({ required_error: "name is required" }).min(5),
+    categoryId: z.string().min(5, {message: 'Please Select a Category'}),
+    tagline: z.string({ required_error: "tagline is required" }).min(5),
+    description: z.string({ required_error: "description is required" }).min(10),
+    thumbnail: z.any().refine((file) => file?.name, {message: 'thumbnail is required'})
+})
+
+export const updateCourseSchema = createCourseSchema.partial({
+    thumbnail: true
+})
