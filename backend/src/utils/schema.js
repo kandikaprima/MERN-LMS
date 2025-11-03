006a66af2d6c38d3c4e3f6aa9147f97fc5ab359e
Kandika Prima Putra <kandikaprima@gmail.com>
2025-11-03 20:54:27 +0800

    feat(schema): enhance validation and add mutateContentSchema
    - Added detailed required_error messages for better form feedback
    - Introduced new mutateContentSchema for course content validation (title, type, youtubeId, text, courseId)
    - Improved clarity and consistency in mutateCourseSchema validation

--- backend/src/utils/schema.js
+++ backend/src/utils/schema.js
@@ -13,8 +13,16 @@ export const signUpSchema = z.object({
 export const signInSchema = signUpSchema.omit({name: true})
 
 export const mutateCourseSchema = z.object({
-    name: z.string().min(5),
-    categoryId: z.string(),
-    tagline: z.string().min(5),
-    description: z.string().min(10)
+    name: z.string({ required_error: "name is required" }).min(5),
+    categoryId: z.string({ required_error: "categoryId is required" }),
+    tagline: z.string({ required_error: "tagline is required" }).min(5),
+    description: z.string({ required_error: "description is required" }).min(10),
+})
+
+export const mutateContentSchema = z.object({
+    title: z.string({ required_error: "title is required" }).min(5),
+    type: z.string({ required_error: "Text/Video is required" }),
+    youtubeId: z.string({ required_error: "Video youtube id is required" }).optional(),
+    text: z.string().optional(),
+    courseId: z.string().min(5)
 })
