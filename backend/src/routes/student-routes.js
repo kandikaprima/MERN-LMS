a744404025e219d74005a66d312bf8df8da701ea
Kandika Prima Putra <kandikaprima@gmail.com>
2025-11-09 02:01:37 +0800

    feat(student): tambah fitur hapus data student dan route DELETE /students/:id
    - Menambahkan fungsi `deleteStudent` pada `student-controller.js` untuk menghapus data student.
    - Menghapus foto student dari direktori `public/uploads/students`.
    - Menghapus referensi student dari koleksi `courseModel`.
    - Menambahkan endpoint `DELETE /students/:id` pada `student-routes.js`.
    - Menjaga konsistensi respons dengan pesan `Delete Student Success`.

--- backend/src/routes/student-routes.js
+++ backend/src/routes/student-routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from "../middlewares/verify-token.js";
-import { getStudent, postStudent, updateStudent } from '../controllers/student-controller.js';
+import { deleteStudent, getStudent, postStudent, updateStudent } from '../controllers/student-controller.js';
 import multer from 'multer';
 import { fileFilter, fileStorage } from '../utils/multer.js';
 
@@ -14,5 +14,6 @@ const upload = multer({
 studentRoutes.get('/students', verifyToken, getStudent)
 studentRoutes.post('/students', verifyToken, upload.single('avatar'), postStudent)
 studentRoutes.put('/students/:id', verifyToken, upload.single('avatar'), updateStudent)
+studentRoutes.delete('/students/:id', verifyToken, deleteStudent)
 
 export default studentRoutes
