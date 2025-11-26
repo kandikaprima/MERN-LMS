440d01327267b321d40091315696f03bdea990b5
Kandika Prima Putra <kandikaprima@gmail.com>
2025-11-26 10:47:21 +0800

    feat: add student assignment route with schema validation
    - Added new POST endpoint `/courses/students/:id` to assign a student to a course
    - Integrated `validateRequest(addStudentCourseSchema)` middleware for input validation
    - Imported and registered new controller `postStudentToCourse`

--- backend/src/routes/course-routes.js
+++ backend/src/routes/course-routes.js
@@ -5,11 +5,14 @@ import {
   getCourses,
   getStudentsByCourseId,
   postCourse,
+  postStudentToCourse,
   updateCourse,
 } from "../controllers/course-controller.js";
 import { verifyToken } from "../middlewares/verify-token.js";
 import multer from "multer";
 import { fileFilter, fileStorageCourse } from "../utils/multer.js";
+import { validateRequest } from "../middlewares/validate-request.js";
+import { addStudentCourseSchema } from "../utils/schema.js";
 
 const courseRoutes = express.Router();
 
@@ -34,5 +37,11 @@ courseRoutes.put(
 );
 courseRoutes.delete("/courses/:id", verifyToken, deleteCourse);
 courseRoutes.get("/courses/students/:id", verifyToken, getStudentsByCourseId);
+courseRoutes.post(
+  "/courses/students/:id",
+  verifyToken,
+  validateRequest(addStudentCourseSchema),
+  postStudentToCourse
+);
 
 export default courseRoutes;
