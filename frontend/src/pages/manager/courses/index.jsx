6f8d99320ddbd9d9d86fb27ba7dd0be3e505f072
Kandika Prima Putra <kandikaprima@gmail.com>
2025-11-27 13:15:05 +0800

    refactor: improve formatting and readability of ManageCoursePage
    - Reformatted the ManageCoursePage component for consistent code style
    - Improved indentation and JSX structure for better clarity

--- frontend/src/pages/manager/courses/index.jsx
+++ frontend/src/pages/manager/courses/index.jsx
@@ -1,38 +1,51 @@
-import React from 'react'
-import { Link, useLoaderData } from 'react-router-dom'
-import CardCourse from './card'
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import CardCourse from "./card";
 
 export default function ManageCoursePage() {
-    const courses = useLoaderData()
+  const courses = useLoaderData();
 
-    return(
-        <>
-        <header className="flex items-center justify-between gap-[30px]">
-                <div>
-                    <h1 className="font-extrabold text-[28px] leading-[42px]">Manage Courses</h1>
-                    <p className="text-[#838C9D] mt-[1]">Give the best future for your great employees</p>
-                </div>
-                <div className="flex items-center gap-3">
-                    <Link to="#" className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap">
-                        Import File
-                    </Link>
-                    <Link to="/manager/courses/create" className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap">
-                        New Course
-                    </Link>
-                </div>
-            </header>
-            <section id="CourseList" className="flex flex-col w-full rounded-[30px] p-[30px] gap-[30px] bg-[#F8FAFB]">
-                {courses?.data?.map((item) => (
-                    <CardCourse 
-                        key={item._id}
-                        category={item.category.name}
-                        id={item._id}
-                        imageUrl={item.thumbnail_url}
-                        name={item.name}
-                        totalStudens={item.total_students}
-                    />
-                ))}
-                {/* <div id="Pagination" className="flex items-center gap-3">
+  return (
+    <>
+      <header className="flex items-center justify-between gap-[30px]">
+        <div>
+          <h1 className="font-extrabold text-[28px] leading-[42px]">
+            Manage Courses
+          </h1>
+          <p className="text-[#838C9D] mt-[1]">
+            Give the best future for your great employees
+          </p>
+        </div>
+        <div className="flex items-center gap-3">
+          <Link
+            to="#"
+            className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap"
+          >
+            Import File
+          </Link>
+          <Link
+            to="/manager/courses/create"
+            className="w-fit rounded-full p-[14px_20px] font-semibold text-[#FFFFFF] bg-[#662FFF] text-nowrap"
+          >
+            New Course
+          </Link>
+        </div>
+      </header>
+      <section
+        id="CourseList"
+        className="flex flex-col w-full rounded-[30px] p-[30px] gap-[30px] bg-[#F8FAFB]"
+      >
+        {courses?.data?.map((item) => (
+          <CardCourse
+            key={item._id}
+            category={item.category.name}
+            id={item._id}
+            imageUrl={item.thumbnail_url}
+            name={item.name}
+            totalStudens={item.total_students}
+          />
+        ))}
+        {/* <div id="Pagination" className="flex items-center gap-3">
                     <button type="button" className="flex shrink-0 w-9 h-9 rounded-full items-center justify-center text-center transition-all duration-300 hover:bg-[#662FFF] hover:text-white hover:border-0 bg-[#662FFF] text-white">
                         <span className="font-semibold text-sm leading-[21px]">1</span>
                     </button>
@@ -49,7 +62,7 @@ export default function ManageCoursePage() {
                         <span className="font-semibold text-sm leading-[21px]">5</span>
                     </button>
                 </div> */}
-            </section>
-        </>
-    )
+      </section>
+    </>
+  );
 }
