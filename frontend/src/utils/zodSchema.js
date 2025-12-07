70d2161819a8da578eb0f71bcc0a007006f7dfb7
Kandika Prima Putra <kandikaprima@gmail.com>
2025-12-07 20:17:41 +0800

    feat: add addStudentCourseSchema for enrolling students into courses

--- frontend/src/utils/zodSchema.js
+++ frontend/src/utils/zodSchema.js
@@ -65,3 +65,7 @@ export const updateStudentSchema = createStudentSchema.omit({
   password: true,
   photo: true,
 });
+
+export const addStudentCourseSchema = z.object({
+  studentId: z.string().min(5),
+});
