d180d9123d803c8650cf0ecfbf6bff1e53422781
Kandika Prima Putra <kandikaprima@gmail.com>
2025-12-14 21:36:25 +0800

    feat: integrate useLoaderData for dynamic overview statistics in ManagerHomePage

--- frontend/src/pages/manager/home/index.jsx
+++ frontend/src/pages/manager/home/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Courses from "./courses";
 import Students from "./students";
+import { useLoaderData } from "react-router-dom";
 
 export default function ManagerHomePage() {
+  const overview = useLoaderData();
+
   return (
     <>
       <header className="flex items-center justify-between gap-[30px]">
@@ -39,7 +42,7 @@ export default function ManagerHomePage() {
               alt="icon"
             />
             <div>
-              <p className="font-extrabold text-2xl leading-[36px]">189,498</p>
+              <p className="font-extrabold text-2xl leading-[36px]">{overview?.totalStudent}</p>
               <p className="text-[#838C9D]">Total Students</p>
             </div>
           </div>
@@ -50,7 +53,7 @@ export default function ManagerHomePage() {
               alt="icon"
             />
             <div>
-              <p className="font-extrabold text-2xl leading-[36px]">7,221</p>
+              <p className="font-extrabold text-2xl leading-[36px]">{overview?.totalCourse}</p>
               <p className="text-[#838C9D]">Total Courses</p>
             </div>
           </div>
@@ -61,7 +64,7 @@ export default function ManagerHomePage() {
               alt="icon"
             />
             <div>
-              <p className="font-extrabold text-2xl leading-[36px]">893,891</p>
+              <p className="font-extrabold text-2xl leading-[36px]">{overview?.totalVideos}</p>
               <p className="text-[#838C9D]">Video Content</p>
             </div>
           </div>
@@ -72,7 +75,7 @@ export default function ManagerHomePage() {
               alt="icon"
             />
             <div>
-              <p className="font-extrabold text-2xl leading-[36px]">12,812</p>
+              <p className="font-extrabold text-2xl leading-[36px]">{overview?.totalTexts}</p>
               <p className="text-[#838C9D]">Text Content</p>
             </div>
           </div>
