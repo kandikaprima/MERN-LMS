14be88f75a7f4807e5081d2f1e26e05c1fbdc6ae
Kandika Prima Putra <kandikaprima@gmail.com>
2025-12-14 22:42:01 +0800

    feat: pass session role from LayoutDashboard to Header

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Sidebar from "./sidebar";
 import Header from "./header";
-import { Outlet, useMatch } from "react-router-dom";
+import { Outlet, useLoaderData, useMatch } from "react-router-dom";
 
 export default function LayoutDashboard({
   isAdmin = true
 }) {
+  const session = useLoaderData();
   const isManagerPreviewPage = useMatch('/manager/courses/:id/preview');
   const isStudentPreviewPage = useMatch('/student/detail-course/:id')
 
@@ -18,7 +19,7 @@ export default function LayoutDashboard({
         <div className="flex min-h-screen">
         <Sidebar isAdmin={isAdmin}/>
         <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
-          <Header />
+          <Header type={session?.role} />
           <Outlet />
         </main>
       </div>
