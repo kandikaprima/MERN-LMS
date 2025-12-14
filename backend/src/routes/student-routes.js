0da3f2fb18af6e72231f6bc81815fbf3af9458f6
Kandika Prima Putra <kandikaprima@gmail.com>
2025-12-14 23:13:00 +0800

    feat: add route to get courses by student
    - Added GET /students-courses endpoint
    - Wired getCoursesByStudentId controller into student routes

--- backend/src/routes/student-routes.js
+++ backend/src/routes/student-routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from "../middlewares/verify-token.js";
-import { deleteStudent, getStudent, getStudentById, postStudent, updateStudent } from '../controllers/student-controller.js';
+import { deleteStudent, getCoursesByStudentId, getStudent, getStudentById, postStudent, updateStudent } from '../controllers/student-controller.js';
 import multer from 'multer';
 import { fileFilter, fileStorage } from '../utils/multer.js';
 
@@ -16,5 +16,6 @@ studentRoutes.get('/students/:id', verifyToken, getStudentById)
 studentRoutes.post('/students', verifyToken, upload.single('avatar'), postStudent)
 studentRoutes.put('/students/:id', verifyToken, upload.single('avatar'), updateStudent)
 studentRoutes.delete('/students/:id', verifyToken, deleteStudent)
+studentRoutes.get('/students-courses', verifyToken, getCoursesByStudentId)
 
 export default studentRoutes
