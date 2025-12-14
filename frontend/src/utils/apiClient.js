c1736eb2ac5c9c0bb684dfa914def28fc47d4908
Kandika Prima Putra <kandikaprima@gmail.com>
2025-12-14 22:17:36 +0800

    feat: add auth response interceptor to handle expired sessions
    - Added response interceptor to authenticated Axios instance
    - Automatically redirect users to manager sign-in on 400 responses
    - Cleared stored session data when authentication fails

--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -26,4 +26,13 @@ apiInstanceAuth.interceptors.request.use((config) => {
   return config
 })
 
+apiInstanceAuth.interceptors.response.use((response) => response, (err) => {
+  if (err?.response?.status === 400) {
+    window.location.replace('/manager/sign-in')
+    secureLocalStorage.removeItem(STORAGE_KEY)
+  }
+
+  return Promise.reject(err)
+})
+
 export default apiInstance
